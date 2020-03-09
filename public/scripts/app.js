f465fb827e208e1630abba7c9a32f1119e64117e
aubreywullschleger <aubreywullschleger@users.noreply.github.com>
2020-03-08 23:29:41 -0500

    extending classes and overriding methods on classes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,21 @@
 "use strict";
 
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _get(target, property, receiver) { if (typeof Reflect !== "undefined" && Reflect.get) { _get = Reflect.get; } else { _get = function _get(target, property, receiver) { var base = _superPropBase(target, property); if (!base) return; var desc = Object.getOwnPropertyDescriptor(base, property); if (desc.get) { return desc.get.call(receiver); } return desc.value; }; } return _get(target, property, receiver || target); }
+
+function _superPropBase(object, property) { while (!Object.prototype.hasOwnProperty.call(object, property)) { object = _getPrototypeOf(object); if (object === null) break; } return object; }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
@@ -32,7 +48,78 @@ var Person = /*#__PURE__*/function () {
   return Person;
 }();
 
-var me = new Person("aubrey w", 30);
-console.log(me.getDescription());
-var other = new Person();
-console.log(other.getDescription());
+var Student = /*#__PURE__*/function (_Person) {
+  _inherits(Student, _Person);
+
+  function Student(name, age, major) {
+    var _this;
+
+    _classCallCheck(this, Student);
+
+    _this = _possibleConstructorReturn(this, _getPrototypeOf(Student).call(this, name, age));
+    _this.major = major;
+    return _this;
+  }
+
+  _createClass(Student, [{
+    key: "hasMajor",
+    value: function hasMajor() {
+      return !!this.major;
+    }
+  }, {
+    key: "getDescription",
+    value: function getDescription() {
+      var description = _get(_getPrototypeOf(Student.prototype), "getDescription", this).call(this);
+
+      if (this.hasMajor()) {
+        description += " Their major is ".concat(this.major, ".");
+      }
+
+      return description;
+    }
+  }]);
+
+  return Student;
+}(Person);
+
+var Traveler = /*#__PURE__*/function (_Person2) {
+  _inherits(Traveler, _Person2);
+
+  function Traveler(name, age, homeLocation) {
+    var _this2;
+
+    _classCallCheck(this, Traveler);
+
+    _this2 = _possibleConstructorReturn(this, _getPrototypeOf(Traveler).call(this, name, age));
+    _this2.homeLocation = homeLocation;
+    return _this2;
+  }
+
+  _createClass(Traveler, [{
+    key: "getGreeting",
+    value: function getGreeting() {
+      var greeting = _get(_getPrototypeOf(Traveler.prototype), "getGreeting", this).call(this);
+
+      if (this.homeLocation) {
+        greeting += " I'm visiting from ".concat(this.homeLocation);
+      }
+
+      return greeting;
+    }
+  }]);
+
+  return Traveler;
+}(Person); // const me = new Person("aubrey w", 30)
+// console.log(me.getDescription())
+// const me = new Student("aubrey w", 30, "advertising")
+// console.log(me.getDescription())
+
+
+var me = new Traveler("aubrey w", 30, "denver");
+console.log(me.getGreeting()); // const other = new Person()
+// console.log(other.getDescription())
+// const other = new Student()
+// console.log(other.getDescription())
+
+var other = new Traveler();
+console.log(other.getGreeting());
--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -11,8 +11,55 @@ class Person {
   }
 }
 
-const me = new Person("aubrey w", 30)
-console.log(me.getDescription())
+class Student extends Person {
+  constructor (name, age, major) {
+    super(name, age)
+    this.major = major
+  }
+  hasMajor() {
+    return !!this.major
+  }
+  getDescription() {
+    let description = super.getDescription()
 
-const other = new Person()
-console.log(other.getDescription())
+    if (this.hasMajor()) {
+      description += ` Their major is ${this.major}.`
+    }
+
+    return description
+  }
+}
+
+class Traveler extends Person {
+  constructor (name, age, homeLocation) {
+    super(name, age)
+    this.homeLocation = homeLocation
+  }
+  getGreeting() {
+    let greeting = super.getGreeting()
+
+    if (this.homeLocation) {
+      greeting += ` I'm visiting from ${this.homeLocation}`
+    }
+
+    return greeting
+  }
+}
+
+// const me = new Person("aubrey w", 30)
+// console.log(me.getDescription())
+
+// const me = new Student("aubrey w", 30, "advertising")
+// console.log(me.getDescription())
+
+const me = new Traveler("aubrey w", 30, "denver")
+console.log(me.getGreeting())
+
+// const other = new Person()
+// console.log(other.getDescription())
+
+// const other = new Student()
+// console.log(other.getDescription())
+
+const other = new Traveler()
+console.log(other.getGreeting())
