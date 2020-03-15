eb4bc42cca0bbedad0d0c9feebbb3d8d7be6d0f2
aubreywullschleger <aubreywullschleger@users.noreply.github.com>
2020-03-14 20:01:28 -0500

    set up webpack

--- public/scripts/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-console.log('app.js'); // JSX - JavaScript XML
-
-var template = React.createElement("p", null, "this is jsx");
-var appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
