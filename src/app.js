cd8e58c10a8e26d8b02d5152e6c4f9e0f8c3d714
aubreywullschleger <aubreywullschleger@users.noreply.github.com>
2020-03-15 20:17:06 -0500

    practice with default exports

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
-// import { square, add } from "./utils"
+// import subtract, { square, add } from "./utils"
 
 // console.log("app.js is running!!")
 // console.log(square(3))
 // console.log(add(2,3))
+// console.log(subtract(100, 80))
 
-import { isAdult, canDrink } from "./person"
+import isSenior, { isAdult, canDrink } from "./person"
 
 console.log(isAdult(18))
 console.log(canDrink(18))
+console.log(isSenior(64))
