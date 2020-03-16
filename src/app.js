c5e329bfe9815b8b651e6afe3ce8799f2d938a31
aubreywullschleger <aubreywullschleger@users.noreply.github.com>
2020-03-15 20:09:09 -0500

    practice with named exports

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
-console.log("app.js is running!!")
+// import { square, add } from "./utils"
+
+// console.log("app.js is running!!")
+// console.log(square(3))
+// console.log(add(2,3))
+
+import { isAdult, canDrink } from "./person"
+
+console.log(isAdult(18))
+console.log(canDrink(18))
