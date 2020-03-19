271f4b3a999728628ba802ea9853957e19ca35d9
aubreywullschleger <aubreywullschleger@users.noreply.github.com>
2020-03-18 21:48:55 -0500

    practice passing jsx as a prop

--- src/app.js
+++ src/app.js
@@ -2,4 +2,21 @@ import React from "react"
 import ReactDOM from "react-dom"
 import IndecisionApp from "./components/IndecisionApp"
 
-ReactDOM.render(<IndecisionApp />, document.getElementById("app"))
+const Layout = (props) => {
+  return (
+    <div>
+      <p>header</p>
+      {props.content}
+      <p>footer</p>
+    </div>
+  )
+}
+
+const template = (
+  <div>
+    <h1>page title</h1>
+    <p>content</p>
+  </div>
+)
+
+ReactDOM.render(<Layout content={template} />, document.getElementById("app"))
