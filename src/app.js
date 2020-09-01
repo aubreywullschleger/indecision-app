b9c9827f635b71da30923390757f0ccf0aeb21ae
aubreywullschleger <aubreywullschleger@users.noreply.github.com>
2020-09-01 15:56:09 -0500

    removed unnecessary comments

--- src/app.js
+++ src/app.js
@@ -5,6 +5,3 @@ import "normalize.css/normalize.css"
 import "./styles/styles.scss"
 
 ReactDOM.render(<IndecisionApp />, document.getElementById("app"))
-
-/* Stretch goals for Indecision App
-1. deploy to web - set up gh, heroku, deploying
